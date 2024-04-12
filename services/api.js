c192c09651b5517b6920db3bca1750d0e0eb101c
Marcelino <marcelino85albuquerque@gmail.com>
2024-04-12 17:43:23 -0300

    termino de projeto blog

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+export const api = axios.create({
+    baseURL: 'https://trtezezrslvdjjenjkmk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRydGV6ZXpyc2x2ZGpqZW5qa21rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjkyMTQ5MCwiZXhwIjoyMDI4NDk3NDkwfQ.3dnj8NMkn80SooU722GiaDKhdSZKFsjy8AkreFMIq6E', 
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRydGV6ZXpyc2x2ZGpqZW5qa21rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjkyMTQ5MCwiZXhwIjoyMDI4NDk3NDkwfQ.3dnj8NMkn80SooU722GiaDKhdSZKFsjy8AkreFMIq6E'
     }
 })
